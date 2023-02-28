4ef06298f54ccbb4cfa4cabf90c1af40caf90c95
Andrey Fedorov <fvandre@outlook.com>
2023-03-01 00:21:44 +0300

    Add java code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,4 +1,9 @@
 public class CreditPaymentService {
 
-    
+    public int calculate(int amount, int termYear, double percent) {
+
+        double payment = (amount * (percent / (100 * 12))) / (1 - Math.pow(1 + (percent / (100 * 12)), -(termYear * 12)));
+
+        return (int) payment;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,17 @@
 public class Main {
 
+    public static void main(String[] args) {
+
+        CreditPaymentService service = new CreditPaymentService();
+
+        int monthlyPayment = service.calculate(1000000, 1, 9.99);
+        System.out.println(monthlyPayment);
+
+        monthlyPayment = service.calculate(1000000, 2, 9.99);
+        System.out.println(monthlyPayment);
+
+        monthlyPayment = service.calculate(1000000, 3, 9.99);
+        System.out.println(monthlyPayment);
+    }
+
 }
